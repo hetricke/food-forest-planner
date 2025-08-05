cc26e3bcb9a14674b4f679d69ed5119eb9a89d55
hetricke <hetricke@umich.edu>
2025-08-05 18:22:48 -0400

    updated package versions

--- planner_functions/inputHandler.py
+++ planner_functions/inputHandler.py
@@ -1,11 +1,18 @@
+import plotly
 
 def cleanInput(width, length):
     try:
         w = float(width)
         l = float(length)
         area = w * l
+
+        if area <= 0 or w <=0 or l <= 0:
+            raise Exception("Width and length must be postitive, non-zero numbers")
+        
         return w, l
     except:
-        raise Exception("Width and length must be numbers")
+        raise Exception("Width and length must be postitive, non-zero numbers")
 
     return w, l
+
+def plotGarden()
